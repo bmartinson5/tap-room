804dd69b1ff2f3c3effc10f6cf4d90451b0f2c7e
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-09-06 08:33:04 -0700

    initial project structure and navbar component

---
+++ tap-room/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar.js'
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ tap-room/src/Navbar.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './css/Navbar.css';
+
+class Navbar extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <div className="App">
+        Tap Room
+      </div>
+    );
+  }
+}
+
+export default Navbar;
