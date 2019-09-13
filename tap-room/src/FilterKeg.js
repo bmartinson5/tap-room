2f3b9bd02ee19ce952afe32d8b021ce93b700fd5
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-09-13 11:52:11 -0700

    can filter kegs by brand, min and max abv

--- tap-room/src/FilterKeg.js
+++ tap-room/src/FilterKeg.js
@@ -8,7 +8,9 @@ export default class KegList extends Component {
     super(props)
     this.state = {
       byName: "",
-      byBrand: ""
+      byBrand: "",
+      byMinAbv: "",
+      byMaxAbv: ""
     }
   }
 
@@ -16,19 +18,39 @@ export default class KegList extends Component {
     this.setState({[evt.target.name]: evt.target.value });
   }
 
+  filterList = (kegList) => {
+    let filteredKegList = kegList.filter(keg =>
+      keg.name.toLowerCase().includes(this.state.byName.toLowerCase()) &&
+      keg.brand.toLowerCase().includes(this.state.byBrand.toLowerCase())
+    )
+
+    if(this.state.byMinAbv !== ""){
+      filteredKegList = kegList.filter(keg => keg.abv >= parseFloat(this.state.byMinAbv))
+    }
+    if(this.state.byMaxAbv !== ""){
+      filteredKegList = kegList.filter(keg => keg.abv <= parseFloat(this.state.byMaxAbv))
+    }
+    return filteredKegList
+  }
+
 
   render(){
     const {kegList, handleClick} = this.props;
-    const filteredKegList = kegList.filter(keg => keg.name.toLowerCase().includes(this.state.byName.toLowerCase()))
-    console.log('filteredList', filteredKegList);
+
+    const filteredKegList = this.filterList(kegList)
 
     return (
       <div>
         <div className="filter-info">
           <h2>Filter by</h2>
-          <input type="text" value={this.state.byName} name="byName" onChange={this.handleChange}placeholder="name" /><br/>
+          Name: <input type="text" value={this.state.byName} name="byName" onChange={this.handleChange}placeholder="name" /><br/>
           <br/>
-          <input type="text" value={this.state.byBrand} name="byBrand" onChange={this.handleChange} placeholder="brand"/><br/>
+          Brand: <input type="text" value={this.state.byBrand} name="byBrand" onChange={this.handleChange} placeholder="brand"/><br/>
+          <br/>
+          Min abv: <input type="number" value={this.state.byMinAbv} name="byMinAbv" onChange={this.handleChange} placeholder="Min abv"/><br/>
+          <br/>
+          Max abv: <input type="number" value={this.state.byMaxAbv} name="byMaxAbv" onChange={this.handleChange} placeholder="Max abv"/><br/>
+          <br/>
 
         </div>
         <div className="keg-list" >
