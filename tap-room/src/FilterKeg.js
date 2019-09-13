9035ace17818cc5c0f73e35dfcec5b54a0993234
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-09-13 11:17:07 -0700

    can filter kegs by name

--- tap-room/src/FilterKeg.js
+++ tap-room/src/FilterKeg.js
@@ -6,18 +6,37 @@ import './css/Keg.css';
 export default class KegList extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      byName: "",
+      byBrand: ""
+    }
+  }
+
+  handleChange = (evt) => {
+    this.setState({[evt.target.name]: evt.target.value });
   }
 
 
   render(){
     const {kegList, handleClick} = this.props;
+    const filteredKegList = kegList.filter(keg => keg.name.toLowerCase().includes(this.state.byName.toLowerCase()))
+    console.log('filteredList', filteredKegList);
 
     return (
-      <div className="keg-list" >
+      <div>
+        <div className="filter-info">
+          <h2>Filter by</h2>
+          <input type="text" value={this.state.byName} name="byName" onChange={this.handleChange}placeholder="name" /><br/>
+          <br/>
+          <input type="text" value={this.state.byBrand} name="byBrand" onChange={this.handleChange} placeholder="brand"/><br/>
 
-        {kegList.map((keg, index) => (
-            <Keg key={index} info={keg} kegNumber={index+1} callback={handleClick}/>
-        ))}
+        </div>
+        <div className="keg-list" >
+
+          {filteredKegList.map((keg, index) => (
+              <Keg key={index} info={keg} kegNumber={index+1} callback={handleClick}/>
+          ))}
+        </div>
       </div>
     );
   }
