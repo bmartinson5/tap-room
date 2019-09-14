694f27cba4e65b4d9237d347154c10026c5ee2ec
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-09-14 15:03:30 -0700

    Updates

---
+++ precache-manifest.3763396521297c72f46249e776d3317c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add87b5053d2248756d64147b7e25ec0",
+    "url": "/tap-room/index.html"
+  },
+  {
+    "revision": "a77112bdffd6ccc73790",
+    "url": "/tap-room/static/css/main.b15a745e.chunk.css"
+  },
+  {
+    "revision": "a8e90ea0789b4757ea84",
+    "url": "/tap-room/static/js/2.96015dd3.chunk.js"
+  },
+  {
+    "revision": "a77112bdffd6ccc73790",
+    "url": "/tap-room/static/js/main.c38ff8bf.chunk.js"
+  },
+  {
+    "revision": "6d3b507fd370da54d51c",
+    "url": "/tap-room/static/js/runtime~main.1e88d29c.js"
+  },
+  {
+    "revision": "c30d1330d866fa9eb7b0aa5f97e89e80",
+    "url": "/tap-room/static/media/keg.c30d1330.jpg"
+  }
+]);
